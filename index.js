74d9b5b8b9a4d88bc5d333baf486fcc16becfb89
Niek Haarman <niek@305.nl>
2020-06-01 13:10:37 +0200

    Include @actions/github sample code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello world!")
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+    const githubToken = core.getInput('github_token');
+
+    const octokit = new github.GitHub(githubToken);
+
+    const { data: pullRequest } = await octokit.pulls.get({
+        owner: 'octokit',
+        repo: 'rest.js',
+        pull_number: 123,
+        mediaType: {
+            format: 'diff'
+        }
+    });
+
+    console.log(pullRequest);
+}
+
+run();
