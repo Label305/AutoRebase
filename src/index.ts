3f9c6108600bb14ba683af147ceacf7df4f550d5
Niek Haarman <niek@305.nl>
2020-06-02 15:43:05 +0200

    Replace custom exec_process.ts with @actions/exec

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import * as execProcess from './exec_process';
 import {setFailed} from '@actions/core';
+import {exec} from '@actions/exec';
 
 async function run() {
     try {
-        await execProcess.execute('touch new_file');
-        const statusResult = await execProcess.execute('git status');
+        await exec('touch new_file');
+        const statusResult = await exec('git status');
         console.log(statusResult);
 
-        const commitResult = await execProcess.execute('git commit -am "New file"');
+        const commitResult = await exec('git commit -am "New file"');
         console.log(commitResult);
 
-        const pushResult = await execProcess.execute('git push');
+        const pushResult = await exec('git push');
         console.log(pushResult);
     } catch (e) {
         setFailed(e);
