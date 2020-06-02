b5d5a99339916a170184f2ea33331784fca7f551
Niek Haarman <niek@305.nl>
2020-06-02 15:35:33 +0200

    Fail CI on promise error

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import * as execProcess from './exec_process';
+import {setFailed} from '@actions/core';
 
 async function run() {
-    await execProcess.execute('touch new_file');
-    const statusResult = await execProcess.execute('git status');
-    console.log(statusResult);
+    try {
+        await execProcess.execute('touch new_file');
+        const statusResult = await execProcess.execute('git status');
+        console.log(statusResult);
 
-    const commitResult = await execProcess.execute('git commit -am "New file"');
-    console.log(commitResult);
+        const commitResult = await execProcess.execute('git commit -am "New file"');
+        console.log(commitResult);
 
-    const pushResult = await execProcess.execute('git push');
-    console.log(pushResult);
+        const pushResult = await execProcess.execute('git push');
+        console.log(pushResult);
+    } catch (e) {
+        setFailed(e);
+    }
 }
 
 run();
