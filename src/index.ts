1c04c3d2e802008533da30ead9835f4f0e9360a5
Niek Haarman <niek@305.nl>
2020-06-02 14:47:54 +0200

    Include prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import {GitHub, context} from "@actions/github";
-import {getInput} from "@actions/core";
+import {GitHub, context} from '@actions/github';
+import {getInput} from '@actions/core';
 
 async function run() {
     const githubToken = getInput('github_token');
     const octokit: GitHub = new GitHub(githubToken);
 
-    const owner = context.repo.owner
-    const repo = context.repo.repo
+    const owner = context.repo.owner;
+    const repo = context.repo.repo;
 
-    const pullsResult = await octokit.pulls.list({owner, repo})
-    const pullsData = pullsResult.data
+    const pullsResult = await octokit.pulls.list({owner, repo});
+    const pullsData = pullsResult.data;
     console.log(pullsData);
 }
 
