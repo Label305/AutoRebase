d036c13b113ceced3eb074110a978c092e152902
Niek Haarman <niek@305.nl>
2020-06-02 16:10:42 +0200

    Rebase the current branch on master

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,9 @@ async function run() {
         await exec('git config user.name "Jenkins305"');
         await exec('git config user.email "joris+jenkins@label305.com"');
 
-        await exec('touch new_file');
-        const statusResult = await exec('git status');
-        console.log(statusResult);
-
-        await exec('git add -A');
-        const commitResult = await exec('git commit -am "New file"');
-        console.log(commitResult);
-
-        const pushResult = await exec('git push');
-        console.log(pushResult);
-
-        await exec('git reset --hard HEAD^');
-        const pushResult2 = await exec('git push --force-with-lease');
-        console.log(pushResult2);
+        await exec('git fetch');
+        const rebaseResult = await exec('git rebase origin/master');
+        console.log(rebaseResult);
     } catch (e) {
         setFailed(e);
     }
