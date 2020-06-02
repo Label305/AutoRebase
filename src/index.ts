6c5c29066655fdc374ce79f92717d08772be393b
Niek Haarman <niek@305.nl>
2020-06-02 13:11:58 +0200

    Properly import using typescript style

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+import {GitHub, context} from "@actions/github";
+import {getInput} from "@actions/core";
 
 async function run() {
-    const githubToken = core.getInput('github_token');
-    const octokit: GitHub = new github.GitHub(githubToken);
+    const githubToken = getInput('github_token');
+    const octokit: GitHub = new GitHub(githubToken);
 
-    const owner = github.context.repo.owner
-    const repo = github.context.repo.repo
+    const owner = context.repo.owner
+    const repo = context.repo.repo
 
     const pullsResult = await octokit.pulls.list({owner, repo})
     const pullsData = pullsResult.data
