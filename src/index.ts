1f4910d5aa25715e366b3a446e0eae82ab98890b
Niek Haarman <niek@305.nl>
2020-06-02 15:26:43 +0200

    Commit the new file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ async function run() {
     await execProcess.execute('touch new_file');
     const statusResult = await execProcess.execute('git status');
     console.log(statusResult);
+
+    const commitResult = await execProcess.execute('git commit -am "New file"');
+    console.log(commitResult);
 }
 
 run();
