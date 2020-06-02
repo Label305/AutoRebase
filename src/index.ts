995cfab66bc3886a3adc40f44ef42793e624714a
Niek Haarman <niek@305.nl>
2020-06-02 14:32:36 +0200

    Include exec_process.ts to run commands on CLI

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import {GitHub, context} from '@actions/github';
-import {getInput} from '@actions/core';
+import * as execProcess from './exec_process';
 
 async function run() {
-    const githubToken = getInput('github_token');
-    const octokit: GitHub = new GitHub(githubToken);
-
-    const owner = context.repo.owner;
-    const repo = context.repo.repo;
-
-    const pullsResult = await octokit.pulls.list({owner, repo});
-    const pullsData = pullsResult.data;
-    console.log(pullsData);
+    const statusResult = await execProcess.execute('git status');
+    console.log(statusResult);
 }
 
 run();
