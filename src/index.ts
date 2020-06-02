138132e4b07e84f7e2b8130812d0f687ba32969f
Niek Haarman <niek@305.nl>
2020-06-02 15:25:41 +0200

    Create a new file during action

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as execProcess from './exec_process';
 
 async function run() {
+    await execProcess.execute('touch new_file');
     const statusResult = await execProcess.execute('git status');
     console.log(statusResult);
 }
